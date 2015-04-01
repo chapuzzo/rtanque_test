4fab721acd9988276e1bd60f809ac1b812141af7
chapuzzo <luismax@gmail.com>
2015-04-01 13:28:03 +0200

    changing setups sequence order

--- install_SDL2.sh
+++ install_SDL2.sh
@@ -4,6 +4,9 @@ mkdir libraries
 cd libraries
 for lib in SDL SDL_ttf SDL_image
 do
+  echo "now cloning ${lib}"
   hg clone https://hg.libsdl.org/${lib}
+  pwd
   cd ${lib} && ./configure && make && sudo make install
+  pwd
 done
