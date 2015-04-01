89e4427a55c94b141d4dd0a6c35930da8bb50fea
chapuzzo <luismax@gmail.com>
2015-04-01 13:14:07 +0200

    splited install sdl2 process to script

---
+++ install_SDL2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir libraries
+cd libraries
+for lib in SDL SDL_ttf SDL_image
+do
+  hg clone https://hg.libsdl.org/${lib}
+  cd ${lib}
+  ./configure
+  make
+  sudo make install
+  cd ..
+done
