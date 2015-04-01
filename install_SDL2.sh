3154be287cf2ccfd03d73083e7bb5a073b8e8e73
chapuzzo <luismax@gmail.com>
2015-04-01 16:17:43 +0200

    different approach to 1.13 to 1.14

--- install_SDL2.sh
+++ install_SDL2.sh
@@ -7,6 +7,6 @@ do
   echo "now cloning ${lib}"
   hg clone https://hg.libsdl.org/${lib}
   pwd
-  ( cd ${lib} && find . -type f  |xargs -I,  sed -i 's/automake-1\.13/automake-1.14/g' , && find . -type f  |xargs -I,  sed -i 's/aclocal-1\.13/aclocal-1.14/g' , && ./configure && make && sudo make install )
+  ( cd ${lib} && find . -type f  |xargs -I,  sed -i 's/-1\.13//g' , && ./configure && make && sudo make install )
   pwd
 done
