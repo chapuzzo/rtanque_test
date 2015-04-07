2235edb0390554e165440595ec13c904b474392b
chapuzzo <luismax@gmail.com>
2015-04-07 14:13:54 +0200

    typo

--- install_SDL2.sh
+++ install_SDL2.sh
@@ -4,7 +4,7 @@ mkdir libraries
 cd libraries
 
 declare -A deps=(\
- [sdl]='https://www.libsdl.org/release/SDL2-2.0.3.tar.gz'\
+ [SDL]='https://www.libsdl.org/release/SDL2-2.0.3.tar.gz'\
  [ttf]='https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz'\
  [image]='https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.0.tar.gz'\
  [mixer]='https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz'\
