ed938894eece1fb7c27090bd06d7c56cacdc3219
chapuzzo <luismax@gmail.com>
2017-06-30 13:24:53 +0200

    bundler instaled as a pre dependency

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-gem install bundler
-
 bundle exec rtanque new_bot test
 bundle exec rtanque start bots/test.rb:x2 --max-ticks 50 --gui=false
 bundle exec rtanque start bots/test.rb:x2 --max-ticks 50
